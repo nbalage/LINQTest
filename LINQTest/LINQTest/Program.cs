4b628bbb9e740e0cc65be74a48e5bf75a142e738
Balazs Nemeth <Balazs_Nemeth@epam.com>
2019-05-06 14:28:46 +0200

    Expression - where, order

--- LINQTest/LINQTest/Program.cs
+++ LINQTest/LINQTest/Program.cs
@@ -13,6 +13,8 @@ namespace LINQTest
         {
             Console.WriteLine(ExpressionSample(3, 5));
 
+            WhereWithExpression();
+
             Console.ReadKey();
         }
 
@@ -22,6 +24,59 @@ namespace LINQTest
             Expression<Func<int, int, int>> add = (x, y) => x + y;
 
             return add.Compile()(a, b);
+        }
+
+        // Ezt helyettesítjük expression-nel:
+        // companies.Where(company => (company.ToLower() == "coho winery" || company.Length > 16)).OrderBy(company => company)
+        private static void WhereWithExpression()
+        {
+            string[] companies = { "Consolidated Messenger", "Alpine Ski House", "Southridge Video", "City Power & Light",
+                   "Coho Winery", "Wide World Importers", "Graphic Design Institute", "Adventure Works",
+                   "Humongous Insurance", "Woodgrove Bank", "Margie's Travel", "Northwind Traders",
+                   "Blue Yonder Airlines", "Trey Research", "The Phone Company",
+                   "Wingtip Toys", "Lucerne Publishing", "Fourth Coffee" };
+
+            // Azért, hogy használhassunk LINQ-t
+            IQueryable<string> queryableData = companies.AsQueryable<string>();
+
+            // létrehozzuk a company változót a lekésérbe
+            ParameterExpression pe = Expression.Parameter(typeof(string), "company");
+
+            // ***** Where(company => (company.ToLower() == "coho winery" || company.Length > 16)) *****
+            Expression left = Expression.Call(pe, typeof(string).GetMethod("ToLower", Type.EmptyTypes)); // a kifejezés bal oldala
+            Expression right = Expression.Constant("coho winery"); // a kifejezés jobb oldala
+            Expression e1 = Expression.Equal(left, right); // maga az ellenőrzés
+
+            // következő kifejezés
+            left = Expression.Property(pe, typeof(string).GetProperty("Length"));
+            right = Expression.Constant(16, typeof(int));
+            Expression e2 = Expression.GreaterThan(left, right);
+
+            Expression predicateBody = Expression.OrElse(e1, e2); // két kikötés összefűzése OR-ral
+
+            MethodCallExpression whereCallExpression = Expression.Call(
+                typeof(Queryable),
+                "Where",
+                new Type[] { queryableData.ElementType },
+                queryableData.Expression,
+                Expression.Lambda<Func<string, bool>>(predicateBody, new ParameterExpression[] { pe }));
+            // ***** End Where *****
+
+            // ***** OrderBy(company => company) *****
+            MethodCallExpression orderByCallExpression = Expression.Call(
+                typeof(Queryable),
+                "OrderBy",
+                new Type[] { queryableData.ElementType, queryableData.ElementType },
+                whereCallExpression,
+                Expression.Lambda<Func<string, string>>(pe, new ParameterExpression[] { pe }));
+            // ***** End OrderBy *****
+
+            IQueryable<string> results = queryableData.Provider.CreateQuery<string>(orderByCallExpression);
+
+            foreach (string company in results)
+            {
+                Console.WriteLine(company);
+            }
         }
     }
 }
