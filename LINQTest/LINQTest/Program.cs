b95a3e85ea30da3218ed60e2ef67039552f488d6
Balazs Nemeth <Balazs_Nemeth@epam.com>
2019-05-06 13:28:52 +0200

    Expression

---
+++ LINQTest/LINQTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ExpressionSample(3, 5));
+
+            Console.ReadKey();
+        }
+
+        private static int ExpressionSample(int a, int b)
+        {
+            Func<int, int> square = x => x * x;
+            Expression<Func<int, int, int>> add = (x, y) => x + y;
+
+            return add.Compile()(a, b);
+        }
+    }
+}
